86eba3ec1ec97ca7619a08febc9df0730bd4f3e1
Aidyn Hillyard <aidyn@example.com>
2022-04-20 16:11:10 -0500

    completed text serializer Co-authored-by: @mpowell773

--- api/serializers/text.py
+++ api/serializers/text.py
@@ -1,1 +1,10 @@
 from rest_framework import serializers
+from ..models.text import Text
+from .user import UserSerializer
+
+class TextSerializer(serializers.ModelSerializer):
+    owner = UserSerializer(many=False, read_only=True)
+
+    class Meta:
+        model = Text
+        fields = ('id', 'title', 'body', 'attribution', 'tags', 'owner')
