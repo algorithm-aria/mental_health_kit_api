569e7a706f93035b13c95793c1e07de0d11c4e5c
Aidyn Hillyard <aidyn@example.com>
2022-04-20 16:23:32 -0500

    completed image serializer Co-authored-by: @mpowell773

--- api/models/__init__.py
+++ api/models/__init__.py
@@ -1,2 +1,3 @@
 from .user import User
 from .text import Text
+from .image import Image
---
+++ api/serializers/image.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+from ..models.image import Image
+from .user import UserSerializer
+
+class ImageSerializer(serializers.ModelSerializer):
+    owner = UserSerializer(many=False, read_only=True)
+
+    class Meta:
+        model = Image
+        fields = ('id', 'title', 'image', 'tags', 'owner')
