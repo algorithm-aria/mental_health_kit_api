1566e71e90fb8b08bc3fc39c2d4172595dcdad2b
Matt Powell <mpowell171@gmail.com>
2022-04-21 16:34:25 -0400

    added paths in urls.py for image and text Co-authored-by: @aidynhillyard

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from api.views.kit import KitView
+from api.views.kit import KitView, TextView, ImageView
 from .views.users import SignUp, SignIn, SignOut, ChangePassword
 
 urlpatterns = [
@@ -8,10 +8,7 @@ urlpatterns = [
     path('sign-in/', SignIn.as_view(), name='sign-in'),
     path('sign-out/', SignOut.as_view(), name='sign-out'),
     path('change-password/', ChangePassword.as_view(), name='change-password'),
-    path('my-kit/', KitView.as_view(), name='my-kit')
-    # GET for landing screen (image/text previews), from there, can POST
-        # POST:
-            # possibly drop-down with Image and Text as options; choosing one will redirect to form
-            # or could have + button at top right of divs to add image / text
-    # GET by ID for individual images or text pieces (you access by clicking from landing), would have separate route, will have additional info (from there, can PUT, DEL)
+    path('my-kit/', KitView.as_view(), name='my-kit'),
+    path('my-kit/text/<int:pk>/', TextView.as_view(), name='my-kit'),
+    path('my-kit/Image/<int:pk>/', ImageView.as_view(), name='my-kit'),
 ]
